98b028d4bf44d66046e8b0b80e4bf9c1cdcce000
Wiktor Jakubczyc <empikempik@gmail.com>
2017-09-27 16:33:29 +0200

    Update run-once-insensitive.cmd

--- run-once-insensitive.cmd
+++ run-once-insensitive.cmd
@@ -1,14 +1,14 @@
-binaries\ptime binaries\ag\ag -ciF "fair game" corpus.txt
-binaries\ptime binaries\ag64\ag -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep1\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep2\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep3\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep4\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep5\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep6\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep7\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\grep8\grep -ciF "fair game" corpus.txt
-binaries\ptime binaries\pt\pt /c /i "fair game" corpus.txt
-binaries\ptime binaries\pt64\pt /c /i "fair game" corpus.txt
-binaries\ptime binaries\sift\sift -cQi "fair game" corpus.txt
-binaries\ptime binaries\sift64\sift -cQi "fair game" corpus.txt
+binaries\ag\ag -ciF "fair game" corpus.txt
+binaries\ag64\ag -ciF "fair game" corpus.txt
+binaries\grep1\grep -ciF "fair game" corpus.txt
+binaries\grep2\grep -ciF "fair game" corpus.txt
+binaries\grep3\grep -ciF "fair game" corpus.txt
+binaries\grep4\grep -ciF "fair game" corpus.txt
+binaries\grep5\grep -ciF "fair game" corpus.txt
+binaries\grep6\grep -ciF "fair game" corpus.txt
+binaries\grep7\grep -ciF "fair game" corpus.txt
+binaries\grep8\grep -ciF "fair game" corpus.txt
+binaries\pt\pt /c /i "fair game" corpus.txt
+binaries\pt64\pt /c /i "fair game" corpus.txt
+binaries\sift\sift -cQi "fair game" corpus.txt
+binaries\sift64\sift -cQi "fair game" corpus.txt
